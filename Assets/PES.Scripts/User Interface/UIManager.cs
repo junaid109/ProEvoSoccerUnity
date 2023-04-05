6ca06ad3413373d2c1edec36d903c28fb0eafec8
Junaid Malik <junmalik109@gmail.com>
2023-04-05 09:16:03 +0100

    Update UIManager.cs

--- Assets/PES.Scripts/User Interface/UIManager.cs
+++ Assets/PES.Scripts/User Interface/UIManager.cs
@@ -1,14 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
 public class UIManager : MonoBehaviour {
+
+
     public Scoreboard scoreboard;
     public GameObject gameOverPanel;
     public Text gameOverText;
 
     public void UpdateScore(Team team) {
-        scoreboard.SetScore(team.teamType, team.score);
+         scoreboard.SetScore(team.teamType, team.score);
     }
 
     public void GameOver(Team winningTeam) {
         gameOverPanel.SetActive(true);
         gameOverText.text = winningTeam.teamType.ToString() + " Team Wins!";
+    }
+
+    internal void ShowGameOverScreen(int playerScore, int opponentScore)
+    {
+        throw new NotImplementedException();
+
+    }
+
+    internal void UpdateOpponentScore(int opponentScore)
+    {
+        
+    }
+
+    internal void UpdatePlayerScore(int playerScore)
+    {
+        throw new NotImplementedException();
     }
 }
