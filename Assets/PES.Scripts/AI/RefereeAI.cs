5e77ec758cbd9b2b5cddea7f118afe953d3872b4
Junaid Malik <junmalik109@gmail.com>
2023-04-13 12:34:08 +0100

    Created basic class to represent RefereeAI

---
+++ Assets/PES.Scripts/AI/RefereeAI.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class RefereeAI : MonoBehaviour {
+    public Team team1;
+    public Team team2;
+    public Ball ball;
+    public GameManager gameManager;
+
+    private void OnCollisionEnter(Collision collision) {
+        // Check for fouls or other infractions
+        if (collision.gameObject.CompareTag("Player")) {
+            var player = collision.gameObject.GetComponent<Player>();
+            var opposingTeam = player.team == team1 ? team2 : team1;
+            if (opposingTeam.HasPossession() && player.IsTackling()) {
+                // Foul committed
+                gameManager.FoulCommitted(player, opposingTeam);
+            }
+        }
+        else if (collision.gameObject.CompareTag("Ball")) {
+            // Check for out of bounds
+            if (!IsInPlay()) {
+                gameManager.OutOfBounds(ball);
+            }
+        }
+    }
+
+    public bool IsInPlay() {
+        return true; // TODO: Implement this method to check if the ball is in play
+    }
+}
